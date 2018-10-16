0e7dba2402da550756cbff38bdd3a7b0dc9e4274
SiNcSaD <generation0987740903@gmail.com>
2018-10-17 03:37:52 +0800

    update

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -5,29 +5,40 @@
       <ul class="uk-child-width-expand" uk-tab="animation: uk-animation-fade">
         <li v-for="tabName in gy.category" :key="tabName"><a href="">{{ tabName }}</a></li>
       </ul>
-
       <!-- Show many albums -->
       <ul class="uk-switcher uk-margin">
         <div v-for="tabName in gy.category" :key="tabName" class="uk-child-width-1-2@s uk-child-width-1-3@m uk-text-center uk-margin-top" uk-grid>
-          <div id="album" v-for="(album, i) in gy.GetCategoryList(tabName)" :key="i">
-            <div class="uk-inline-clip uk-transition-toggle" tabindex="0" uk-toggle="target: #modal-close-default">
+          <div id="album" v-for="(album, i) in gy.GetCategoryList(tabName)" :key="i" v-on:click="OpenAlbum(album)" href="#modal-full" uk-toggle>
+            <div class="uk-inline-clip uk-transition-toggle" tabindex="0">
               <img :src="album.cover">
               <div class="uk-transition-fade uk-position-cover uk-overlay uk-overlay-default uk-flex uk-flex-column uk-flex-center uk-flex-middle">
                 <div class="uk-transition-slide-top-small"><h4 class="uk-margin-remove">{{ album.title }}</h4></div>
                 <div class="uk-transition-slide-bottom-small"><h4 class="uk-margin-remove">{{ album.date }}</h4></div>
               </div>
             </div>
-            <div id="modal-close-default" uk-modal>
-                <div class="uk-modal-dialog uk-modal-body">
-                    <button class="uk-modal-close-default" type="button" uk-close></button>
-                    <h2 class="uk-modal-title">Default</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-            </div>
           </div>
         </div>
       </ul>
-      <!-- light box -->
+    </div>
+    <!-- modal-full region -->
+    <div id="modal-full" class="uk-modal-full" uk-modal>
+      <div class="uk-modal-dialog">
+        <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
+        <!-- specify album viewport -->
+        <div class="uk-background-muted uk-padding uk-panel" uk-height-viewport>
+          <h2 v-if="showAlbum" class="uk-modal-title">{{ showAlbum.title }}</h2>
+          <div v-if="showAlbum">
+            <gallery v-bind:images="showAlbum.images" :index="index" @close="index = null"></gallery>
+            <div
+              class="image"
+              v-for="(image, imageIndex) in showAlbum.images"
+              :key="imageIndex"
+              @click="index = imageIndex"
+              :style="{ backgroundImage: 'url(' + image + ')', width: '275px', height: '200px' }"
+            ></div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -41,6 +52,14 @@ class Album {
     this.date = date
     this.cover = '/static/Gallery/' + category + '/' + title + '/1.jpg'
     this.numMax = numMax
+    this.images = this.GetImages()
+  }
+  GetImages () {
+    var imgs = []
+    for (let num = 1; num <= this.numMax; num++) {
+      imgs.push('/static/Gallery/' + this.category + '/' + this.title.replace(/\s+/g, '%20') + '/' + num + '.jpg')
+    }
+    return imgs
   }
 }
 
@@ -65,34 +84,39 @@ export default {
     return {
       gy: new GY(
         [
-          new Album('學術活動', 'IEEE IGRSS Taipei Chapter 頒獎', '2018109', 23),
+          new Album('學術活動', 'IEEE IGRSS Taipei Chapter 頒獎', '2018109', 22),
           new Album('學術活動', '日本學者短期訪問', '20180615', 23),
           new Album('實驗室聚會', '1062期末歡送會', '20170506', 1),
           new Album('Seminar', '金融科技Fintech區塊鏈介紹', '20170927', 2),
-          new Album('Seminar', 'Pattern recoginition related topics', '20171018', 2),
-          new Album('Seminar', 'Educational strategies for learning English', '20171122', 2),
-          new Album('Seminar', '神經網路影像處理', '20181129', 2),
-          new Album('Seminar', 'Device-Free Non-Privacy Invasive Activity Monitoring of Elderly People in A Smart House', '20181227', 2),
-          new Album('Seminar', '海洋汙染防治', '20180110', 2),
-          new Album('Seminar', 'AI Architecture', '20180314', 2),
-          new Album('Seminar', '輔助機器人', '20180321', 2),
-          new Album('Seminar', '合成孔徑雷達', '20180328', 2),
-          new Album('Seminar', '太空衛星科普 福衛三號為例', '20180411', 2),
-          new Album('Seminar', 'GPU在測繪上的應用', '20180502', 2)
+          new Album('Seminar', 'Pattern recoginition related topics', '20171018', 3),
+          new Album('Seminar', 'Educational strategies for learning English', '20171122', 4),
+          new Album('Seminar', '神經網路影像處理', '20181129', 7),
+          new Album('Seminar', 'Device-Free Non-Privacy Invasive Activity Monitoring of Elderly People in A Smart House', '20181227', 6),
+          new Album('Seminar', '海洋汙染防治', '20180110', 7),
+          new Album('Seminar', 'AI Architecture', '20180314', 3),
+          new Album('Seminar', '輔助機器人', '20180321', 3),
+          new Album('Seminar', '合成孔徑雷達', '20180328', 3),
+          new Album('Seminar', '太空衛星科普 福衛三號為例', '20180411', 3),
+          new Album('Seminar', 'GPU在測繪上的應用', '20180502', 3),
+          new Album('105級口試', '蕭志宇', '201807xx', 11),
+          new Album('105級口試', '邱立承', '201807xx', 9),
+          new Album('105級口試', '任哲威', '201807xx', 9),
+          new Album('105級口試', '陳柏堯', '201807xx', 27),
+          new Album('105級口試', '王以文', '201807xx', 7)
         ],
-        ['學術活動', 'Seminar', '實驗室聚會']
+        ['學術活動', 'Seminar', '實驗室聚會', '105級口試']
       ),
-      images: [
-        'https://dummyimage.com/800/ffffff/000000',
-        'https://dummyimage.com/1600/ffffff/000000',
-        'https://dummyimage.com/1280/000000/ffffff',
-        'https://dummyimage.com/400/000000/ffffff'
-      ],
-      index: null
+      index: null,
+      showAlbum: null
     }
   },
   components: {
     'gallery': VueGallery
+  },
+  methods: {
+    OpenAlbum: function (album) {
+      this.showAlbum = album
+    }
   }
 }
 </script>
@@ -113,5 +137,13 @@ export default {
   font-size: 24px;
   font-weight: bold;
   color: black;
+}
+.image {
+  float: left;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  border: 1px solid #ebebeb;
+  margin: 5px;
 }
 </style>
